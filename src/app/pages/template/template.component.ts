57f2c303a9e33426e9c7704424089a1d465bbd91
nidia <nidiacmh@gmail.com>
2021-01-15 17:24:09 -0600

    validaciones en el template

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
-
+usuario={
+  nombre: '',
+  apellido:'',
+  correo:''
+}
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(forma: NgForm){
+    console.log(forma);
+    console.log(forma.value);
   }
 
 }
