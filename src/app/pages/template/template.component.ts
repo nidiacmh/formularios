1038ea0e89ec038fb61aee739a12fd81873528a4
nidia <nidiacmh@gmail.com>
2021-01-18 16:28:30 -0600

    servicio para obtener los paises del mundo que hablan español

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {PaisService} from '../../services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -12,13 +13,23 @@ usuario={
   apellido:'',
   correo:''
 }
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.paisService.getPaises()
+        .subscribe( paises => {
+          console.log(paises);
+        });
   }
 
   guardar(forma: NgForm){
     console.log(forma);
+    if (forma.invalid){
+      Object.values(forma.controls).forEach(control =>{
+        control.markAsTouched();
+      });
+      return;
+    }
     console.log(forma.value);
   }
 
