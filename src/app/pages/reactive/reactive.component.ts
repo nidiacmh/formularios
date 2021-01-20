7fb51bae53854ce39effe7d5e21b7b5680637bbc
nidia <nidiacmh@gmail.com>
2021-01-20 15:33:52 -0600

    validacion de formulario

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -18,9 +18,9 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario(){
     this.forma =  this.fb.group({
-      nombre: ['Nidia'],
-      apellido: ['Meza'],
-      correo: ['nidia@gmail.com'],
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      correo: ['',[ Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
     });
   }
   guardar(){
