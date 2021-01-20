de000a576b328e6e591646f8dd473d64deca2a11
nidia <nidiacmh@gmail.com>
2021-01-20 15:45:42 -0600

    validacion de formulario html y boton guardar

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -16,6 +16,16 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario(){
     this.forma =  this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
@@ -24,6 +34,13 @@ export class ReactiveComponent implements OnInit {
     });
   }
   guardar(){
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
     console.log(this.forma);
   }
+
+
 }
