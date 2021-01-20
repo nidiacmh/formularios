fe0bfd094739140ea9e05131a1d37ca8ea285677
nidia <nidiacmh@gmail.com>
2021-01-20 15:56:25 -0600

    creacion de campos anidados

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -31,6 +31,10 @@ export class ReactiveComponent implements OnInit {
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(3)]],
       correo: ['',[ Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
+      direccion:this.fb.group({
+        distrito:['', Validators.required],
+        ciudad: ['',Validators.required],
+      }),
     });
   }
   guardar(){
