9125fc60e132f081b0c6212d64ffe01c0e311e61
nidia <nidiacmh@gmail.com>
2021-01-28 15:55:20 -0600

    validacion personalizada

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -39,18 +39,30 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
   }
 
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+    return (pass1 === pass2) ? false : true;
+  }
+
   crearFormulario(){
     this.forma =  this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
       apellido: ['', [Validators.required, Validators.minLength(3), this.validadores.noMeza]],
       correo: ['',[ Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
+      pass1: ['',[Validators.required]],
+      pass2: ['',[Validators.required]],
       direccion:this.fb.group({
         distrito:['', Validators.required],
         ciudad: ['',Validators.required],
       }),
-      pasatiempos: this.fb.array([
-
-      ])
+      pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
     });
   }
 
@@ -58,7 +70,7 @@ export class ReactiveComponent implements OnInit {
     // this.forma.setValue({
     this.forma.reset({
       nombre:'Nidia',
-      apellido:'Meza',
+      apellido:'Hernandez',
       correo:'nidia@gmail.com',
       direccion:{
         distrito:'Puebla',
--- src/app/services/validadores.service.ts
+++ src/app/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,17 @@ export class ValidadoresService {
       }
     }
     return null;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+    return (formGruop: FormGroup) => {
+      const pass1Control = formGruop.controls[pass1Name];
+      const pass2Control = formGruop.controls[pass2Name];
+      if(pass1Control.value === pass2Control.value){
+        pass2Control.setErrors(null);
+      }else{
+        pass2Control.setErrors({noEsIgual: true});
+      }
+    }
   }
 }
